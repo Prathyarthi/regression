61be539bc0b807f5ce76915839c09b0e9fb96719
Prathyarthi <prathyarti@gmail.com>
2024-01-05 13:35:27 +0530

    predicted accuracy and also mse,mae,rmse

--- regression.py
+++ regression.py
@@ -37,3 +37,32 @@ print(regressor.intercept_)     # For intercept
 
 plt.plot(X_train,regressor.predict(X_train),"r")   # To create best fit line
 plt.show()
+
+
+# Predicting the test results
+y_pred = regressor.predict(X_test)
+print(y_pred,y_test)
+# test_dataset
+# plt.scatter(X_test,y_test)
+# plt.plot(X_test,regressor.predict(X_test),"r")
+# plt.show()
+
+
+
+# Performance metrics -> R squared and adjusted R squared
+# Cost functions -> MSE, MAE, RMSE
+
+from sklearn.metrics import mean_squared_error,mean_absolute_error
+mse = mean_squared_error(y_test,y_pred)   # mse between y_test and y_pred
+mae = mean_absolute_error(y_test,y_pred)
+rmse = np.sqrt(mse)
+print(mse)
+print(mae)
+print(rmse)
+
+# To get the score of the model:
+from sklearn.metrics import r2_score
+score = r2_score(y_test,y_pred)
+print("Accuracy :",score)
+
+  
